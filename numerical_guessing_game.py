191639492df4dbb9a74359805ddcfc8b8bfe25ee
Rustam Guliyev <86050915+rustam24052003@users.noreply.github.com>
2023-07-04 20:06:33 +0400

    first commit

---
+++ numerical_guessing_game.py
@@ -0,0 +1,32 @@
+import random
+
+num = random.randint(1, 100)
+print("Welcome to Numerical Guessing Game")
+
+
+def is_valid(n):
+    return n.isdigit() and 1 <= int(n) <= 100
+
+
+def is_num_valid():
+    while True:
+        guess = input()
+        if is_valid(guess):
+            return int(guess)
+        else:
+            print("Please use number in range [0,100]")
+
+
+def compare_num():
+    while True:
+        n = is_num_valid()
+        if n > num:
+            print("Please type number less than yours")
+        elif n < num:
+            print("Please type number greater than yours")
+        else:
+            print("Good job,you guessed it")
+            break
+
+
+compare_num()
